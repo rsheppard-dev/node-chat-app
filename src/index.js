e1f880bd4ae50c5050722f452760f7710ad08983
rsheppard83 <rsheppard83@gmail.com>
2021-06-01 16:16:54 +0100

    added broadcast feature for users joining and leaving

--- src/index.js
+++ src/index.js
@@ -17,9 +17,14 @@ io.on('connection', socket => {
   console.log('New WebSocket connection.')
 
   socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined the chat!')
 
   socket.on('sendMessage', message => {
     io.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat!')
   })
 })
 
