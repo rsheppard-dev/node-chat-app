6b0dec88fdb1ed45b3a4954db4675584d7191df3
rsheppard83 <rsheppard83@gmail.com>
2021-06-02 21:47:06 +0100

    added bad-word filter and geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,49 @@
 const socket = io()
 
 const form = document.getElementById('chat-form')
+const messageInput = form.querySelector('input')
+const messageButton = form.querySelector('button')
+const locationButton = document.getElementById('share-location')
+
+socket.on('message', message => {
+    console.log(message)
+})
 
 form.addEventListener('submit', e => {
     e.preventDefault()
 
-    const message = e.target.elements.message.value
+    messageButton.setAttribute('disabled', 'disabled')
 
-    socket.emit('sendMessage', message)
+    const message = messageInput.value
+
+    socket.emit('sendMessage', message, error => {
+        messageButton.removeAttribute('disabled')
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
+
     form.reset()
+    messageInput.focus()
 })
 
-socket.on('message', message => {
-    console.log(message)
+locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser!')
+    }
+
+    locationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition(position => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            locationButton.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
 })
